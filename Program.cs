d5dd4f887ca4194df57ee9dbd6c71538de14d3f5
Pham Nhu Quoc Trieu <phamnhuquoctrieu307@gmail.com>
2024-09-16 19:40:42 +0700

    aad file

---
+++ Program.cs
@@ -0,0 +1,11 @@
+ namespace LogChallenge
+{
+class Program
+    {
+        static void Main(string[] args)
+            {
+            Console.WriteLine(Hello World!);
+            Console.WriteLine(Log Challenge!);
+        }
+    }
+} 
