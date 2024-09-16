804f353a6d1c1535c56cecf49e126a861dec876d
Pham Nhu Quoc Trieu <phamnhuquoctrieu307@gmail.com>
2024-09-16 19:41:35 +0700

    Thay doi lan 1

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
- namespace LogChallenge
+namespace LogChallenge
 {
-class Program
-    {
-        static void Main(string[] args)
-            {
-            Console.WriteLine(Hello World!);
-            Console.WriteLine(Log Challenge!);
-        }
-    }
-} 
+public static class Program
+{
+public static void Main(string[] args)
+{
+
+Using the Log
+
+
+var calculator = new Calculator.Calculator();
+Console.WriteLine($"The square root of 2 is
+{calculator.SquareRoot(2)}");
+}
+}
+}
